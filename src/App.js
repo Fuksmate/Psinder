3128a3cc2030e4c22a92fbf83961fa50ae7f1158
Lukasz Fuchs <lukaszfuchs18@gmail.com>
2022-08-09 02:21:22 +0200

    add coments and todo for tomorow

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import "./styles/App.css";
 import Card from "./components/card/card";
 import Aside from "./components/aside/aside";
+/*
+ TODO Stworzyć wiadomość
+ TODO Stworzyć Aside z profilami
+ TODO Stworzyć Profil
+ TODO O'Skryptową button adoptuj i odrzuć oraz informacje
+
+ ! Zadanie dodatkowe
+ ! Rozkwilić jak ogarnąć ten backend
+ 
+*/
 
 function App() {
   return (
